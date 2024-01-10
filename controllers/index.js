f68ea6db65be71cc4d46f00ad0a9937d09f266cc
Tomasz Pawlikowski <tp4458@gmail.com>
2024-01-10 13:12:39 +0000

    added controllers/index.js

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dash-routes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashRoutes);
+
+module.exports = router;
