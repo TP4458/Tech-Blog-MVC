cca9eec3b827896c36e5efad8af7a8b4c867f2e5
Tomasz Pawlikowski <tp4458@gmail.com>
2024-01-16 00:57:45 +0000

    fixed filename error

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Authentication for login/signup
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({ where: { email: req.body.email } });
+    if (!dbUserData) {
+      res.status(400).json({ message: 'Incorrect email or password' });
+      return;
+    }
+
+    const validatePass = await userData.checkPassword(req.body.password);
+
+    if (!validatePass) {
+      res.status(400).json({ message: 'Incorrect email or password' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.logged_in = true;
+      res.json({ user: dbUserData, message: 'You are now logged in' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// logout
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
