3cf76f71a4f65cc0a7de710594c366604c055ef5
Tomasz Pawlikowski <tp4458@gmail.com>
2024-01-16 00:36:33 +0000

    dash route

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
+const sequelise = require('../config/connection');
 const { Post, User, Comment } = requrie('..models');
 const withAuth = requrie('../utils/auth');
+
+router.get('/editPost/:id', withAuth, async (req, res) => {
+  try {
+    const editPost = await Post.findByPk(req.params.id);
+    console.log(editPost);
+
+    if (editPost) {
+      const post = editPost.get({ plain: true });
+
+      res.render('editPost', {
+        ...post,
+        logged_in: true,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
