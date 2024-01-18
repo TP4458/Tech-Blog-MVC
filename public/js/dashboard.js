862b2fe253593d870526d3a916faf357b9407c6d
Tomasz Pawlikowski <tp4458@gmail.com>
2024-01-18 14:38:21 +0000

    dashboard handler

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+  const title = document.querySelector('#post-name').value.trim();
+  const content = document.querySelector('#post-desc').value.trim();
 
+  // create new post
+  if (title && content) {
+    const response = await fetch(`/api/blogs`, {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create a new post.');
+    }
+  }
+};
+
+// delete an existing post
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+// event listener for buttons
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
