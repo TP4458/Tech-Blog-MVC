8becf6c2267c30df2d147de77df46513f8fa9a02
Tomasz Pawlikowski <tp4458@gmail.com>
2024-01-26 02:23:16 +0000

    updated seeding method

--- seeds/index.js
+++ seeds/index.js
@@ -1,22 +1,51 @@
-const seedPost = require('./postSeed');
-const seedUser = require('./userSeed');
-const seedComm = require('./commSeed');
+// const seedPost = require('./postSeed');
+// const seedUser = require('./userSeed');
+// const seedComm = require('./commSeed');
+
+// const sequelize = require('../config/connection');
+
+// // seeding the schema
+// const seedAll = async () => {
+//   await sequelize.sync({ force: true });
+//   console.log('\n--- DATABASE SYNCED ---\n');
+
+//   await seedUser();
+//   console.log('\n--- USERS SEEDED ---\n');
+
+//   await seedPost();
+//   console.log('\n--- POSTS SEEDED ---\n');
+
+//   await seedComm();
+//   console.log('\n--- COMMENTS SEEDED ---\n');
+
+//   process.exit(0);
+// };
+
+// seedAll();
 
 const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const postSeed = require('./postSeed.json');
+const userSeed = require('./userSeed.json');
+const commSeed = require('./commSeed.json');
 
-// seeding the schema
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n--- DATABASE SYNCED ---\n');
+  const users = await User.bulkCreate(userSeed, {
+    individualHooks: true,
+    returning: true,
+  });
 
-  await seedUser();
-  console.log('\n--- USERS SEEDED ---\n');
+  // console.log(users)
+  // const userData = users.map((user)=>user.get({plain:true}))
+  //console.log(userData)
 
-  await seedPost();
-  console.log('\n--- POSTS SEEDED ---\n');
+  const postInfo = await Post.bulkCreate(postSeed);
+  // const postData = postInfo.map((post) => post.get({ plain: true }));
 
-  await seedComm();
-  console.log('\n--- COMMENTS SEEDED ---\n');
+  // console.log(postData)
+
+  const comments = await Comment.bulkCreate(commSeed);
 
   process.exit(0);
 };
